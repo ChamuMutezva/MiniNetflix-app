667131c09cbbb1e1bbc7ea0c693b26231c444454
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2019-11-16 17:03:27 +0200

    duplicate code added to environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAf28iNu9mwMgrf-fXkz9wJnSnbXKjDdJI",
+    authDomain: "mininetflix-app.firebaseapp.com",
+    databaseURL: "https://mininetflix-app.firebaseio.com",
+    projectId: "mininetflix-app",
+    storageBucket: "mininetflix-app.appspot.com",
+    messagingSenderId: "446970091111",
+    appId: "1:446970091111:web:dbee2b5d6aab670357a6c2",
+    measurementId: "G-XNW0G10R6H"
+  }
 };
